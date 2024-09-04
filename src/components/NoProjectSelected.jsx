557ac7edfce9270b0b40a638ebe56b7daffbf598
Herry-Jacker <herryjacker4@gmail.com>
2024-09-04 14:51:25 +0630

    Modified: ProjectSidebar.jsx to show UI project list and Added: onStartAddProject() functiion to add new project buttons

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,6 +1,6 @@
 import noProjectImg from '../assets/no-projects.png';
 import Button from './Button';
-export default function NoProjectSelected() {
+export default function NoProjectSelected({onStartAddProject}) {
     return (
         <div className="mt-24 text-center w-2/3">
             <img src={noProjectImg}
@@ -9,7 +9,7 @@ export default function NoProjectSelected() {
             <h2 className='text-xl font-bold text-stone-500 my-4'>No Project Selected</h2>
             <p className='text-stone-400 mb-4'>Select a project or get started with a new one</p>
             <p className='mt-8'>
-                <Button>Create new project</Button>
+                <Button onClick={() => onStartAddProject()} >Create new project</Button>
             </p>
         </div>
     )
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,15 +1,27 @@
 import Button from "./Button";
 
-export default function ProjectSidebar() {
+export default function ProjectSidebar({onStartAddProject, projects, onSelectProject, selectedProjectId}) {
     return (
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
             <h2>Sidebar</h2>
             <div>
-                <Button>
+                <Button onClick={() => onStartAddProject()} >
                     + Add New Project
                 </Button>
             </div>
-            <ul></ul>
+            <ul className="mt-8">
+                {projects.map(project => {
+                    let classes = "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800";
+                    if (project.id === selectedProjectId) {
+                        classes += " bg-stone-800 text-stone-200";
+                    } else {
+                        classes += " text-stone-400"
+                    }
+                    return <li key={project.id}>
+                        <button onClick={() => onSelectProject(project.id)} className={classes}>{project.title}</button>
+                    </li>
+                })}
+            </ul>
         </aside>
     )
 }
