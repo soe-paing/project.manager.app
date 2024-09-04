be5c6a708af5d73ebfac4d9a791570b5e9b18fb0
Herry-Jacker <herryjacker4@gmail.com>
2024-09-04 14:41:02 +0630

    Modified with forwardRef and accept ref

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,16 @@
-export default function Input({label, textarea}) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input({label, textarea, ...props}, ref) {
     const classes = "w-full p-1 border-b-2 rounded-sm border-stion-300 bg-stone-200 text-stone-600 focus:border-stone-600";
     return (
         <p className="flex flex-col gap-1 my-4">
             <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
-            {textarea ? <textarea className={classes} /> : <input className={classes} />}
+            {
+                textarea ? <textarea {...props} ref={ref} className={classes} />
+                : <input {...props} ref={ref} className={classes} />
+            }
         </p>
     )
-}
+})
+
+export default Input;
