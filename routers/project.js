b29ba554db095cea8a71ed2aab6da8ec351abaa8
herry-jacker <herryjacker4@gmail.com>
2024-09-12 15:45:26 +0630

    Added: more api route

--- routers/project.js
+++ routers/project.js
@@ -15,8 +15,8 @@ router.get("/projects", async function (req, res) {
 });
 
 router.get("/projects/:id", async function (req, res) {
+    const { id } = req.params;
     try {
-        const { id } = req.params;
         const data = await prisma.project.findUnique({
             where: { id: Number(id) },
             include: { tasks: true },
@@ -26,5 +26,39 @@ router.get("/projects/:id", async function (req, res) {
         res.status(500).json({ error: e})
     }
 });
+
+// curl -X POST localhost:8000/projects -d title="Some Content" description="Some Description" dueDate="09-12-2024"
+router.post("/projects", async function (req, res) {
+    const { title, description, dueDate } = req.body;
+    try {
+        if(!(title || description || dueDate)) {
+            return res.status(400).json({ msg: "content required" });
+        }
+        const project = await prisma.project.create({
+            data: {
+                title, description, dueDate
+            },
+            include: {
+                tasks: true,
+            }
+        })
+        res.status(201).json(project);
+    } catch (e) {
+        res.status(500).json({ error: e})
+    }
+})
+
+// curl -X DELETE localhost:8000/projects/8
+router.delete("/projects/:id", async function (req, res) {
+    const { id } = req.params;
+    try {
+        await prisma.project.delete({
+            where: { id: Number(id) },
+        })
+        res.status(204);
+    } catch (e) {
+        res.status(500).json({ error: e})
+    }
+})
 
 export const projectRouter = router;
