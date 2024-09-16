6287a7c8e1d8780645267736500e5b3ee3598a12
Herry-Jacker <herryjacker4@gmail.com>
2024-09-16 16:56:28 +0630

    modifiled: App.jsx remove fetching code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { useQuery } from "react-query";
 import ProjectSidebar from "./components/ProjectSidebar";
 import { Outlet } from "react-router-dom";
 
-const api = "http://localhost:8000/projects";
-
-async function fetchProject() {
-    const res = await fetch(api);
-    return res.json();
-}
-
-
 function App() {
-  const { data, isLoading, isError, error} = useQuery("projects", fetchProject);
-
-  if (isError) {
-		return <div>{error}</div>;
-	}
-
-  if (isLoading) {
-		return <div>Loading...</div>;
-	}
-
   return (
     <main className="h-screen my-8 flex gap-4 pr-4">
-      <ProjectSidebar data={data} />
+      <ProjectSidebar />
       <Outlet />
     </main>
   );
